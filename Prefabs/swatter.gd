0cd7dc6099f2f1163afd57b49e725c96575ac478
Randy Butts <randybuttsii@gmail.com>
2023-12-03 00:12:37 -0500

    Finished swatter

--- Prefabs/swatter.gd
+++ Prefabs/swatter.gd
@@ -2,9 +2,15 @@ extends Node2D
 
 @export var SWING_TIME: float = 0.5
 @export var RESET_TIME: float = 1
-@onready var ROTATION_LIMIT: float = rotation
+
+@onready var Holder: Node2D = $Holder
+@onready var ROTATION_LIMIT: float = Holder.rotation
 
 var CAN_SWING: bool = true
+
+func _ready():
+	for node in get_children():
+		print(node.name)
 
 func _on_area_2d_body_entered(body: Node2D):
 	if body is PlayerCharacter:
@@ -18,9 +24,11 @@ func swing():
 	var tween = create_tween()
 	tween.tween_property($Holder, "rotation", ROTATION_LIMIT * -1, SWING_TIME)
 	tween.tween_callback(reset_swing)
+	print("Swinging to "+str(ROTATION_LIMIT * -1))
 
 
 func reset_swing():
 	var tween = create_tween()
 	tween.tween_property($Holder, "rotation", ROTATION_LIMIT, RESET_TIME)
 	tween.tween_callback(func(): CAN_SWING = true)
+	print("Resetting to "+str(ROTATION_LIMIT))
